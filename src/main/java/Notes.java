4d8a55c3d66084c8e83e3615583cd5b4c195c82b
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-27 14:44:24 +0000

    update files

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,14 +1,45 @@
 
 class Notes {
+
+	// constants - variables that can't be changed
+
+	// written in all capital letters, use underscores between words
+	final static int MAXIMUM_VALUE = 15;
+
 	public static void main(String[] args) {
 
-		//constants
+		// compareTo - comparing non-primitive data types
 
-		//compareTo
+		String word = "Help";
+		String word2 = "hello";
 
-		//switch
+		System.out.println(word.compareTo(word2));
+		// negative - before alphabetically (ASCII value is before)
+		// zero - same
+		// positive - after alphabetically (ASCII value is after)
 
-		
+		// switch - way too many ifs
+
+		int num = 4;
+		switch (num) {
+			case 1:
+				System.out.println("case 1");
+				break;
+			case 2:
+				System.out.println("case 2");
+				break;
+			case 3:
+				System.out.println("case 3");
+				break;
+			case 4:
+				System.out.println("case 4");
+				break;
+			case 5:
+				System.out.println("case 5");
+				break;
+			default: //else statement
+				System.out.println("default");
+		}
 
 	}
 }
