40a32847479fb0842c4b345eaa3426014407e8ab
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-02-27 15:33:35 +0000

    finit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,8 @@
 public class PracticeProblem {
 	public static void main(String args[]) {
 		System.out.println(totalWordsChecker("Hello() b c d e f g h i j"));
+		System.out.println(("A").compareTo("B"));
+		System.out.println(("B").compareTo("A"));
 	}
 
 	public static int calculate(int arg1, int arg2, char operator) {
@@ -31,8 +33,8 @@ public class PracticeProblem {
 		}
 	}
 
+public static final int MAXIMUM = 10;
 	public static boolean totalWordsChecker(String sentence) {
-		final int MAXIMUM = 10;
 		int wordCount = 1;
 		for (int i = 0; i < sentence.length(); i++) {
 			switch (sentence.charAt(i)) {
@@ -45,8 +47,10 @@ public class PracticeProblem {
 				case '[':
 					wordCount += 1;
 					break;
-				case ')':
+				case '(':
+				if (sentence.charAt(i+1) != ')') {
 					wordCount += 1;
+				}
 					break;
 				case '-':
 					wordCount += 1;
@@ -75,7 +79,22 @@ public class PracticeProblem {
 		return wordCount <= MAXIMUM;
 	}
 
+	public static String minString(String s1, String s2, String s3) {
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+		s3 = s3.toLowerCase();
+		if (s1.compareTo(s2) < 0 && s1.compareTo(s3) < 0) {
+			return s1;
+		} else if (s2.compareTo(s1) < 0 && s2.compareTo(s3) < 0) {
+			return s2;
+		} else if (s3.compareTo(s1) < 0 && s3.compareTo(s2) < 0) {
+			return s3;
+		}
+		return "WHAAAT";
+	}
 }
+
+
 
 /*
  * [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-
